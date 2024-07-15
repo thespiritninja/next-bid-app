3cfa29bd7950f8345cd0f85db15b2bfbc7589cef
Shreyas Sawant <shreyassawant018@gmail.com>
2024-07-15 02:04:54 -0400

    setup nextauth and db adapters

--- src/env.ts
+++ src/env.ts
@@ -5,10 +5,14 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
     NODE_ENV: z.string().min(1),
+    AUTH_GOOGLE_ID: z.string().min(1),
+    AUTH_GOOGLE_SECRET: z.string().min(1),
   },
   client: {},
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
+    AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
   },
 });
